ff170f033deeda0fde90695733449cdc9224e5f7
AlaaAldinHajjar <alaahajjar66@gmail.com>
2023-11-15 18:50:01 +0300

    new

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,30 @@
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        try(FileInputStream in = new FileInputStream("input.txt");
-            FileOutputStream out = new FileOutputStream("output.txt"))
-        {
+        try(FileInputStream in = new FileInputStream("input.txt")) {
             byte[] buffer = new byte[in.available()];
             in.read(buffer, 0, buffer.length);
-            out.write(buffer, 0, buffer.length);
-        } catch(FileNotFoundException ex) {
-            System.out.println("One of files does not exist or you do not have permissions to I/O");
-            System.out.println(ex.getMessage());
-        } catch(Exception ex) {
-            System.out.println(ex.getMessage());
+
+            String[] input = new String(buffer).split(" ");
+            int a = Integer.parseInt(input[0]);
+            int b = Integer.parseInt(input[1]);
+            System.out.println(a / b);
+        } catch(FileNotFoundException e) {
+            System.out.println("input file is not found");
+        } catch(NumberFormatException e) {
+            System.out.println("error during parsing integer");
+        } catch(ArrayIndexOutOfBoundsException e) {
+            System.out.println("file has less than two integers");
+        } catch(ArithmeticException e) {
+            System.out.println("divide by zero");
+        } catch(IOException e) {
+            System.out.println("error during I/O operations");
+        } catch(Exception e) {
+            System.out.println(e.getMessage());
         }
     }
 }
